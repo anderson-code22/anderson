c6ed09c74ae7e03dfef98263258cfbdd66d0f53f
anderson-code22 <andersongamasilva5@gmail.com>
2024-12-13 12:55:03 -0300

    Arquivos

---
+++ script.js
@@ -0,0 +1,4 @@
+const hambugurger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+
+hambugurger.addEventListener("click", () => nav.classList.toggle("active"));
